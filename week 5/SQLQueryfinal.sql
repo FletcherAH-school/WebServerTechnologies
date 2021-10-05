0c94ba470bd80e081e24b9eba4574d6802a0dbc5
FletcherAH-school <fhenders4836@gmail.com>
2021-10-04 21:30:40 -0600

    week 5

---
+++ week 5/SQLQueryfinal.sql
@@ -0,0 +1,28 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+--SELECT TOP (1000) [firstname]
+--    ,[lastname]
+--    ,[username]
+-- FROM [First Database].[dbo].[Users]
+
+
+--INSERT INTO Users (firstname, lastname, username)
+--VALUES ('beep', 'boop','bop')
+
+
+--SELECT * FROM Users
+-- INNER JOIN addresses
+-- ON Users.username = addresses.username
+
+
+--SELECT firstname, lastname, username
+--FROM [First Database].[dbo].[Users]
+
+
+--DELETE FROM Users
+--WHERE firstname = 'jim'
+
+
+--UPDATE Users SET username = 'beans'
+--WHERE username = 'bop'
+
+--lookup table would be the columns? I'm honestly not sure and will ask in class to clear up:)
